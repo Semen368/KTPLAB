183dcef3fa6172d2a450c38c8a9bd2e2cc140275
Semen368 <78819210+Semen368@users.noreply.github.com>
2021-06-04 09:11:37 +0300

    Add files via upload

---
+++ lab3/Location.java
@@ -0,0 +1,42 @@
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+public class Location
+{
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+
+    @Override
+    public boolean equals(Object a)
+    {
+        if(a instanceof Location)
+        {
+            return ((((Location) a).xCoord==this.xCoord) && (((Location) a).yCoord==this.yCoord)) ? true : false;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return 37*(37 + this.xCoord)+this.yCoord;
+    }
+}
